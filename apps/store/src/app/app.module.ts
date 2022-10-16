c87438556c183d87b5a3b0563529155c78a94960
Jonathan Hogate <jonathan.hogate@jbhunt.com>
2022-10-16 13:45:29 -0500

    lab 6

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent, FormatRatingPipe } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, FormatRatingPipe],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    FormatRatingPipe
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'game/:id',
+          loadChildren: () =>
+            import('@bg-hoard/store/feature-game-detail').then(
+              (m) => m.StoreFeatureGameDetailModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
     MatCardModule,
-    StoreUiSharedModule
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { storeFeatureGameDetailRoutes } from './lib.routes';
+import { GameDetailComponent } from './game-detail/game-detail.component';
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(storeFeatureGameDetailRoutes), MatCardModule],
+  declarations: [GameDetailComponent],
+})
+export class StoreFeatureGameDetailModule {}
