338ce53d7bdc970beb7fa8cf36f83c13e8bbbf10
Jonathan Hogate <jonathan.hogate@jbhunt.com>
2022-10-16 12:59:56 -0500

    lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { MatCardModule } from '@angular/material/card';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    MatCardModule
+    MatCardModule,
+    StoreUiSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
